f34e9c607d8371ab8a387e4161710f8482372092
Nicole Kirkbride <nicolekirkbride@macbookair.home>
2025-04-02 17:10:11 +0100

    Introduced Experience Section and WIderLearning components

---
+++ src/Components/ExperienceSection.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function ExperienceSection() {
+  const experienceList = [
+    {
+      title: "QA Level 4 Software Developer Apprenticeship",
+      company: "Hargreaves Services plc",
+      duration: "1.5 yrs",
+      description: "",
+    },
+    {
+      title: "Future Talent Programme",
+      company: "Tesla",
+      duration: "6mo",
+      description: "",
+    },
+    {
+      title: "First Class BSc (Hons) in Physics",
+      company: "Newcastle University",
+      duration: "3 yrs",
+      description: "",
+    },
+  ];
+  return <div></div>;
+}
+
+export default ExperienceSection;
